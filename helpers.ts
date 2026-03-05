76c3a5849c4154ae109136eefdeb541fad3772d2
kingsman3429 <119696597+kingsman3429@users.noreply.github.com>
2026-03-05 15:47:42 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T12:46:49.185Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T12:47:43.615Z
+Commit: Patch API endpoint error handling
