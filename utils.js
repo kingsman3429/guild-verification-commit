1abd9937963e0baea12affa679c420ea84481a5d
kingsman3429 <119696597+kingsman3429@users.noreply.github.com>
2026-03-05 15:49:55 +0300

    Fix race condition in async operations

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T12:47:35.416Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T12:49:56.890Z
+Commit: Fix race condition in async operations
